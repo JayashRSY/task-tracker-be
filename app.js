0c4b04d8b3679e6c3f6d5f78b682bc1c755aafd7
Jayash Raj Singh Yadav <65907404+JayashRSY@users.noreply.github.com>
2023-09-06 11:24:40 +0530

    cors fix

--- app.js
+++ app.js
@@ -13,9 +13,6 @@ const colors = require('colors')
 
 const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }));
-
 const corsOptions = {
     origin: 'https://task-tracker-fe-mu.vercel.app/', // Replace with your actual frontend domain
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -23,6 +20,11 @@ const corsOptions = {
     optionsSuccessStatus: 204, // Some legacy browsers (IE11) choke on a 204 response, so 204 should be returned
 };
 app.use(CORS(corsOptions));
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 // app.use(CORS({ origin: 'http://localhost:4200' }))
 // app.use((req, res, next) => {
 //     res.setHeader(
